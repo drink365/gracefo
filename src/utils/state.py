3017f5c2d5f7fd22b151d1b87dad69025e1a36a4
drink365 <166272199+drink365@users.noreply.github.com>
2025-10-05 11:26:19 +0800

    Delete src directory

--- src/utils/state.py
+++
@@ -1,8 +0,0 @@
-
-import streamlit as st
-
-def init_state():
-    defaults = {"asset_items": []}
-    for k,v in defaults.items():
-        if k not in st.session_state:
-            st.session_state[k] = v
